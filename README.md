be62568485f57b9ab3f0a4ffe9008993ffb16a22
TenthYoung <tenthyoung@gmail.com>
2019-08-13 14:27:42 -0700

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# Face-Detection-App
-An app that allows your to submit a photo, and using the Clarifai API, the website will locate the face in the photo.
-
-## Technologies used:
-* ReactJS
-* Clarifai
-* Tachyons
-* ParticlesJS
---
+++ precache-manifest.7729fa9e624bf0bc1644c947d3d5dd24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "cecc33c1f63d688e08df",
+    "url": "./static/js/main.cecc33c1.chunk.js"
+  },
+  {
+    "revision": "9424b0be9237d7c045b8",
+    "url": "./static/js/2.9424b0be.chunk.js"
+  },
+  {
+    "revision": "cecc33c1f63d688e08df",
+    "url": "./static/css/main.b9cd2acb.chunk.css"
+  },
+  {
+    "revision": "9424b0be9237d7c045b8",
+    "url": "./static/css/2.b4a03d41.chunk.css"
+  },
+  {
+    "revision": "502855946e7a3d97faa09f8496a2dc9a",
+    "url": "./index.html"
+  }
+];
